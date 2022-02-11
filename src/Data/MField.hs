9e032919d966aacf82889b9ab360105e386651a7
Andrey Mulik <work.a.mulik@gmail.com>
2022-02-11 19:02:12 +0300

    changed GMField definition

--- src/Data/MField.hs
+++ src/Data/MField.hs
@@ -16,7 +16,7 @@ module Data.MField
   module Data.Field,
   
   -- * Mutable field
-  GMField (..), MField, MFieldC (..), pattern (:=@), pattern (:~@),
+  GMField (..), MField, MFieldC (..), (=:),
   MGetter, MSetter, MModifier, MModifierM,
   getter, setter, modifier, modifierM,
   
@@ -44,7 +44,7 @@ default ()
   specific record. As a rule, it makes sense to store 'MFieldRef' in the record
   to which it refers.
 -}
-newtype GMFieldRef m e cs = GMFieldRef {fromGMFieldRef :: Var m (FObject (MFieldC m e) cs)}
+newtype GMFieldRef m e as = GMFieldRef {fromGMFieldRef :: Var m (FObject (MFieldC m e) as)}
 
 {- |
   'MFieldRef' is a structure containing accessors for a specific field of a
@@ -167,114 +167,89 @@ type MModifierM m e = Var m ((e -> m e) -> m e)
       print =<< get field record
   @
 -}
-newtype GMField m record e as = GMField {fromGMField :: record -> Var m (GMFieldRef m e as)}
+newtype GMField m record e as = GMField {fromGMField :: record -> GMFieldRef m e as}
   deriving ( Typeable )
 
 -- | 'GMField' synonym with getter, setter and modifiers.
 type MField m record e = GMField m record e [FieldGetA, FieldSetA, FieldModifyA, FieldModifyMA]
 
-instance MonadVar m => IsField (GMField m record e as) (FieldC m record e) FieldGetA
+instance MonadVar m => IsField (GMField m record e (as :: [Symbol])) (FieldC m record e) FieldGetA
   where
     type FieldGetA ~?= GMField m record e as = FieldGetA ~:= as
     
-    fromField (GMField f) = FieldGetA $ join.get this <=< getRef <=< get this.f
+    fromField (GMField f) = FieldGetA $ join.get this <=< getRef.f
 
 instance MonadVar m => IsField (GMField m record e as) (FieldC m record e) FieldSetA
   where
     type FieldSetA ~?= GMField m record e as = FieldSetA ~:= as
     
-    fromField (GMField f) = FieldSetA $ ap (get this <=< setRef <=< get this.f)
+    fromField (GMField f) = FieldSetA . flip $ \ val ->
+      ($ val) <=< get this <=< setRef.f
 
 instance MonadVar m => IsField (GMField m record e as) (FieldC m record e) FieldModifyA
   where
     type FieldModifyA ~?= GMField m record e as = FieldModifyA ~:= as
     
-    fromField (GMField f) = FieldModifyA $ ap (get this <=< modifyRef <=< get this.f)
+    fromField (GMField f) = FieldModifyA . flip $ \ g ->
+      ($ g) <=< get this <=< modifyRef.f
 
 instance MonadVar m => IsField (GMField m record e as) (FieldC m record e) FieldModifyMA
   where
     type FieldModifyMA ~?= GMField m record e as = FieldModifyMA ~:= as
     
-    fromField (GMField f) = FieldModifyMA $ ap (get this <=< modifyMRef <=< get this.f)
+    fromField (GMField f) = FieldModifyMA . flip $ \ g ->
+      ($ g) <=< get this <=< modifyMRef.f
 
 --------------------------------------------------------------------------------
 
 -- | 'GMField' mutable getter.
 getter :: (MonadVar m, FieldGetA ~:= as) => GMField m record e as
        -> Field m record (m e)
-getter (GMField f) =
-  let Field g s m mm = this
-  in  Field (g <=< getRef <=< get this.f)
-   (ap' (flip  s) (getRef <=< get this.f))
-   (ap' (flip  m) (getRef <=< get this.f))
-   (ap' (flip mm) (getRef <=< get this.f))
+getter (GMField f) = Field  (getRecord     this <=< getRef.f)
+      (flip $ \ x -> ($ x) . setRecord     this <=< getRef.f)
+      (flip $ \ x -> ($ x) . modifyRecord  this <=< getRef.f)
+      (flip $ \ x -> ($ x) . modifyRecordM this <=< getRef.f)
 
 -- | 'GMField' mutable setter.
 setter :: (MonadVar m, FieldSetA ~:= as) => GMField m record e as
        -> Field m record (e -> m ())
-setter (GMField f) =
-  let Field g s m mm = this
-  in  Field (g <=< setRef <=< get this.f)
-   (ap' (flip  s) (setRef <=< get this.f))
-   (ap' (flip  m) (setRef <=< get this.f))
-   (ap' (flip mm) (setRef <=< get this.f))
+setter (GMField f) = Field  (getRecord     this <=< setRef.f)
+      (flip $ \ x -> ($ x) . setRecord     this <=< setRef.f)
+      (flip $ \ x -> ($ x) . modifyRecord  this <=< setRef.f)
+      (flip $ \ x -> ($ x) . modifyRecordM this <=< setRef.f)
 
 -- | 'GMField' mutable modifier (pure).
 modifier :: (MonadVar m, FieldModifyA ~:= as) => GMField m record e as
          -> Field m record ((e -> e) -> m e)
-modifier (GMField f) =
-  let Field g s m mm = this
-  in  Field (g <=< modifyRef <=< get this.f)
-   (ap' (flip  s) (modifyRef <=< get this.f))
-   (ap' (flip  m) (modifyRef <=< get this.f))
-   (ap' (flip mm) (modifyRef <=< get this.f))
+modifier (GMField f) = Field  (getRecord     this <=< modifyRef.f)
+        (flip $ \ x -> ($ x) . setRecord     this <=< modifyRef.f)
+        (flip $ \ x -> ($ x) . modifyRecord  this <=< modifyRef.f)
+        (flip $ \ x -> ($ x) . modifyRecordM this <=< modifyRef.f)
 
 -- | 'GMField' mutable modifier (monadic).
 modifierM :: (MonadVar m, FieldModifyMA ~:= as) => GMField m record e as
           -> Field m record ((e -> m e) -> m e)
-modifierM (GMField f) =
-  let Field g s m mm = this
-  in  Field (g <=< modifyMRef <=< get this.f)
-   (ap' (flip  s) (modifyMRef <=< get this.f))
-   (ap' (flip  m) (modifyMRef <=< get this.f))
-   (ap' (flip mm) (modifyMRef <=< get this.f))
+modifierM (GMField f) = Field (getRecord     this <=< modifyMRef.f)
+        (flip $ \ x -> ($ x) . setRecord     this <=< modifyMRef.f)
+        (flip $ \ x -> ($ x) . modifyRecord  this <=< modifyMRef.f)
+        (flip $ \ x -> ($ x) . modifyRecordM this <=< modifyMRef.f)
 
 --------------------------------------------------------------------------------
+
+(=:) :: (Typeable m, Typeable record, Typeable e, Typeable as, MonadVar m)
+     => GMField m record e as -> record -> GMField m record e as
+     -> Prop m (GMField m record e as) record
+field =: record = Prop . LinkField field record
 
 data LinkProp m field record
   where
-    LinkFields :: MonadVar m => !(GMField m record e as) -> !(GMField m record e as)
-                             -> LinkProp m (GMField m record e as) record
-    
-    LinkAccess :: MonadVar m => !(GMField m record e as) -> !(GMField m record e as)
-                             -> LinkProp m (GMField m record e as) record
+    LinkField :: MonadVar m
+      => !(GMField m record e as) -> record -> !(GMField m record e as)
+      -> LinkProp m (GMField m record e as) record
 
 instance IsProp LinkProp
   where
-    performProp record (LinkFields (GMField ref1) (GMField ref2)) =
-      setField this (ref1 record) =<< get this (ref2 record)
-    
-    performProp record (LinkAccess (GMField ref1) (GMField ref2)) = do
-      ref1' <- fromGMFieldRef <$> get this (ref1 record)
-      setField this ref1' =<< get this.fromGMFieldRef =<< get this (ref2 record)
-
--- | Connect first field with second.
-pattern (:=@) :: (MonadVar m, Typeable m, Typeable record, Typeable e, Typeable as)
-              => GMField m record e as -> GMField m record e as
-              -> Prop m (GMField m record e as) record
-pattern x :=@ y = Prop (LinkFields x y)
-
--- | Connect first field accessors with current second field accessors.
-pattern (:~@) :: (MonadVar m, Typeable m, Typeable record, Typeable e, Typeable as)
-              => GMField m record e as -> GMField m record e as
-              -> Prop m (GMField m record e as) record
-pattern x :~@ y = Prop (LinkAccess x y)
-
---------------------------------------------------------------------------------
-
-ap :: Monad m => (a -> m (b -> m c)) -> a -> b -> m c
-ap f = \ x y -> do f' <- f x; f' y
-
-ap' :: Monad m => (c -> b -> m d) -> (a -> m b) -> a -> c -> m d
-ap' f g = \ x y -> f y =<< g x
+    performProp record1 (LinkField (GMField field1) record2 (GMField field2)) =
+      setRecord this (fromGMFieldRef $ field1 record1) <=<
+      getRecord this . fromGMFieldRef $ field2 record2
 
